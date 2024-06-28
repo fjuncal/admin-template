d454e797306333f3dc7a8f69d1bb5a9aeb73eb2b
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-28 16:21:29 -0300

    Acertando o css do componente de Layout

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -10,9 +10,12 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   return (
-    <div className={`flex`}>
+    <div className={`flex h-screen w-screen`}>
       <MenuLateral />
-      <div>
+      <div
+        className={`flex flex-col w-full p-7
+          bg-gray-300`}
+      >
         <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
         <Conteudo>{props.children}</Conteudo>
       </div>
