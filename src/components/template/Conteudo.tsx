1c569f6c6f3c8f7c9953cfca84019b8d62ab703d
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-28 16:04:24 -0300

    Criando a estrutura inicial da aplicação, criando os componentes de layout

---
+++ src/components/template/Conteudo.tsx
@@ -0,0 +1,17 @@
+import MenuLateral from "./MenuLateral";
+
+interface ConteudoProps {
+  children?: any;
+}
+
+export default function Conteudo(props: ConteudoProps) {
+  return (
+    <div
+      className={`
+        flex flex-col mt-7
+    `}
+    >
+      {props.children}
+    </div>
+  );
+}
