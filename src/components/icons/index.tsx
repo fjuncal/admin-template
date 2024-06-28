798f9f7a723dec561b35eb587eeafef192fe7b7f
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-28 18:44:46 -0300

    Finalizando o componente BotaoAlternarTema, deixando ele compativel para tamanho de tela

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -68,14 +68,14 @@ export const IconeSair = (
   </svg>
 );
 
-export const IconeSol = (
+export const IconeSol = (tamanho = "6") => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="size-6"
+    className={`size-${tamanho}`}
   >
     <path
       strokeLinecap="round"
@@ -84,14 +84,14 @@ export const IconeSol = (
     />
   </svg>
 );
-export const IconeLua = (
+export const IconeLua = (tamanho = "6") => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="size-6"
+    className={`size-${tamanho}`}
   >
     <path
       strokeLinecap="round"
