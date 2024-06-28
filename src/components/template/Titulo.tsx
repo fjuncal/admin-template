523ee60db63fd6af25e728cd7ece6c9b89af72f9
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-28 16:51:14 -0300

    Acertando CSS do componente do titulo

--- src/components/template/Titulo.tsx
+++ src/components/template/Titulo.tsx
@@ -10,14 +10,14 @@ export default function Titulo(props: TituloProps) {
     <div>
       <h1
         className={`
-        
+        font-black text-3xl text-gray-900
         `}
       >
         {props.titulo}
       </h1>
       <h2
         className={`
-        
+        font-light text-sm text-gray-600
         `}
       >
         {props.subtitulo}
