c82872d1210bf51693a387d8605d644a45aa958f
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-28 16:13:18 -0300

    adicionando fonte no projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import Layout from "@/components/template/Layout";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-green-500  to-blue-500">
-      <h1>Admin</h1>
-    </div>
+    <Layout
+      titulo="Página Incial"
+      subtitulo="Estamos construindo um template admin"
+    >
+      <h3>Conteudo</h3>
+    </Layout>
   );
 }
