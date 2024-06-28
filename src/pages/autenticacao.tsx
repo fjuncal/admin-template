05c4ec60cff4b60b31838af925e3029b11427287
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-28 19:33:57 -0300

    Adicionando link no final da pagina e finalizando CSS da pagina de autenticacao

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -17,17 +17,17 @@ export default function Autenticacao() {
   }
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className={`hidden md:block w-1/2`}>
+      <div className={`hidden md:block md:w-1/2 lg:2/3`}>
         <Image
           src={foto}
           alt="Imagem da Tela de Autenticação"
           className="h-screen w-full object-cover"
         />
       </div>
-      <div className="m-10 w-1/2">
+      <div className="m-10 w-full md:w-1/2 lg:1/3`">
         <h1
           className={`
-        text-xl font-bold mb-5
+        text-3xl font-bold mb-5
         `}
         >
           {modo === "login"
@@ -64,6 +64,36 @@ export default function Autenticacao() {
         >
           Entrar com Google
         </button>
+
+        {modo === "login" ? (
+          <p className="mt-8">
+            Novo por aqui?
+            <a
+              onClick={() => setModo("cadastro")}
+              className={`
+                text-blue-500 hover:text-blue-700 font-semibold
+                cursor-pointer
+                `}
+            >
+              {" "}
+              Crie uma conta
+            </a>
+          </p>
+        ) : (
+          <p className="mt-8">
+            Já possui conta?
+            <a
+              onClick={() => setModo("login")}
+              className={`
+                text-blue-500 hover:text-blue-700 font-semibold
+                cursor-pointer
+                `}
+            >
+              {" "}
+              Entre com as suas credenciais
+            </a>
+          </p>
+        )}
       </div>
     </div>
   );
