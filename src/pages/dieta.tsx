2385d563297c52284749f96bd6c227dda3890451
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-28 23:36:16 -0300

    Criando tela de dieta

---
+++ src/pages/dieta.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/template/Layout";
+
+export default function Dieta() {
+  return (
+    <Layout
+      titulo="Ajustes e configurações"
+      subtitulo="Personalize o sistema por aqui!"
+    >
+      <h3>Conteudo!!</h3>
+    </Layout>
+  );
+}
