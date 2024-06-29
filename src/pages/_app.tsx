17a33b7dfcf084ca888d2f0db51c1d620f81194e
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-29 00:44:00 -0300

    Adiconando menu de escolha do dia para a tela de dieta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-cyan/theme.css";
+import Tailwind from "primereact/passthrough/tailwind";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PrimeReactProvider>
+    <PrimeReactProvider value={{ pt: Tailwind }}>
       <AppProvider>
         <Component {...pageProps} />
       </AppProvider>
--- src/pages/dieta.tsx
+++ src/pages/dieta.tsx
@@ -1,12 +1,33 @@
 import Layout from "@/components/template/Layout";
+import { SelectButton } from "primereact/selectbutton";
+import { useState } from "react";
 
 export default function Dieta() {
+  const [diasDaSemana] = useState([
+    { label: "Segunda", value: "segunda" },
+    { label: "Terça", value: "terca" },
+    { label: "Quarta", value: "quarta" },
+    { label: "Quinta", value: "quinta" },
+    { label: "Sexta", value: "sexta" },
+    { label: "Sábado", value: "sabado" },
+    { label: "Domingo", value: "domingo" },
+  ]);
+  const [size, setSize] = useState(diasDaSemana[1].value);
   return (
     <Layout
-      titulo="Ajustes e configurações"
-      subtitulo="Personalize o sistema por aqui!"
+      titulo="Dieta da Semana"
+      subtitulo="Consulte pelo dia da semana a dieta do dia!!"
     >
-      <h3>Conteudo!!</h3>
+      <div className="card">
+        <SelectButton
+          value={size}
+          onChange={(e) => setSize(e.value)}
+          options={diasDaSemana}
+          className={`flex justify-center items-center
+            text-sm
+            `}
+        />
+      </div>
     </Layout>
   );
 }
