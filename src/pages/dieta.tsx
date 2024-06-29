c6b3eb2e1b51ed0e1aca60f337e4a88a681ffeb6
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-29 04:06:12 -0300

    Adicionando cor para o tema escuro

--- src/pages/dieta.tsx
+++ src/pages/dieta.tsx
@@ -9,43 +9,50 @@ export default function Dieta() {
       <span className="justify-center items-center isolate inline-flex rounded-md shadow-sm">
         <button
           type="button"
-          className="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          className="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold  ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 text-gray-600
+        dark:text-gray-200 dark:hover:bg-gray-800 dark:bg-gray-600"
         >
           Seg
         </button>
         <button
           type="button"
-          className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10text-gray-600
+        dark:text-gray-200 dark:hover:bg-gray-800 dark:bg-gray-600"
         >
           Ter
         </button>
         <button
           type="button"
-          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10text-gray-600
+        dark:text-gray-200 dark:hover:bg-gray-800 dark:bg-gray-600"
         >
           Quar
         </button>
         <button
           type="button"
-          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10text-gray-600
+        dark:text-gray-200 dark:hover:bg-gray-800 dark:bg-gray-600"
         >
           Qui
         </button>
         <button
           type="button"
-          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 text-gray-600
+        dark:text-gray-200 dark:hover:bg-gray-800 dark:bg-gray-600"
         >
           Sex
         </button>
         <button
           type="button"
-          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 text-gray-600
+        dark:text-gray-200 dark:hover:bg-gray-800 dark:bg-gray-600"
         >
           Sáb
         </button>
         <button
           type="button"
-          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10 text-gray-600
+        dark:text-gray-200 dark:hover:bg-gray-800 dark:bg-gray-600"
         >
           Dom
         </button>
