5faa23bfd259826087355945e6e61185d22bc133
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-29 02:47:37 -0300

    Retirando PrimeReact do projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
 import { AppProvider } from "@/data/context/AppContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <PrimeReactProvider>
-      <AppProvider>
-        <Component {...pageProps} />
-      </AppProvider>
-    </PrimeReactProvider>
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
   );
 }
