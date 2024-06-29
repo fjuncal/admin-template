8e2408f8b355e5db42479a55de117e8ada12198b
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-29 03:56:13 -0300

    Adicionando botão de dias de semana

--- src/pages/dieta.tsx
+++ src/pages/dieta.tsx
@@ -5,6 +5,51 @@ export default function Dieta() {
     <Layout
       titulo="Dieta da Semana"
       subtitulo="Consulte pelo dia da semana a dieta do dia!!"
-    ></Layout>
+    >
+      <span className="justify-center items-center isolate inline-flex rounded-md shadow-sm">
+        <button
+          type="button"
+          className="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        >
+          Seg
+        </button>
+        <button
+          type="button"
+          className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        >
+          Ter
+        </button>
+        <button
+          type="button"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        >
+          Quar
+        </button>
+        <button
+          type="button"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        >
+          Qui
+        </button>
+        <button
+          type="button"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        >
+          Sex
+        </button>
+        <button
+          type="button"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        >
+          Sáb
+        </button>
+        <button
+          type="button"
+          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        >
+          Dom
+        </button>
+      </span>
+    </Layout>
   );
 }
